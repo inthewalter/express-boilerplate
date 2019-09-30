b05ee9665ca35f7dd0edce301117425a0104b5c9
inthewalter <21400564@handong.edu>
2019-09-30 20:57:32 +0900

    Init project for boilerplate
    매번 번거로운 셋팅을 피하기 위해서 보일러플레이트를 제작하는 중입니다.

---
+++ src/api/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const userRoutes = require('./user.route');
+
+/**
+ * GET v1/status
+ */
+router.get('/status', (req, res) => res.send('OK'));
+
+/**
+ * GET v1/docs
+ */
+router.use('/docs', express.static('docs'));
+
+router.use('/user', userRoutes);
+
+router.get('/', (req, res) => {
+  console.log('ㅇㅅㅇ')
+  res.render('index');
+})
+module.exports = router;
